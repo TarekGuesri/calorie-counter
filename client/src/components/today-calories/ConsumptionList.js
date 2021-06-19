cc36b3e4bf601302dc76dc5a55f6526860448dcc
TarekGuesri <tguesri@gmail.com>
2021-06-19 18:02:55 +0100

    Added ConsumptionList component

---
+++ client/src/components/today-calories/ConsumptionList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ConsumptionList = () => {
+  return <div>ConsumptionList</div>;
+};
+
+export default ConsumptionList;
--- client/src/pages/TodayCalories.js
+++ client/src/pages/TodayCalories.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+
+import { WEBSITE_NAME } from 'src/utils/brand';
+import ConsumptionList from 'src/components/today-calories/ConsumptionList';
 
 const TodayCalories = () => {
-  return <div>TodayCalories</div>;
+  return (
+    <>
+      <Helmet>
+        <title>{`${WEBSITE_NAME} - Today's Calories`}</title>
+      </Helmet>
+      <h3> {"Today's Calories"}</h3>
+      <ConsumptionList />
+    </>
+  );
 };
 
 export default TodayCalories;
