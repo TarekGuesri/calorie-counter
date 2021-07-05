765d848649f6e704ede9712f1274dc8d3319c8cc
TarekGuesri <tguesri@gmail.com>
2021-07-05 17:40:04 +0100

    Changed AddConumption's style

--- client/src/components/today-calories/AddConsumption.js
+++ client/src/components/today-calories/AddConsumption.js
@@ -2,34 +2,55 @@ import React, { useState } from 'react';
 import ReactAutocomplete from 'react-autocomplete';
 
 const AddConsumption = () => {
-  const [state, setState] = useState({ value: '' });
+  const [state, setState] = useState({ id: '', value: '' });
+
+  const onSelect = (value, item) => {
+    console.log(value);
+    console.log(item);
+    setState({ value });
+  };
   return (
-    <div>
-      AddConsumption{' '}
+    <div className="mb-5">
+      <h5 style={{ display: 'inline' }}> Add a consumption </h5>
+      (if you want to add a new food click <a href="#">here</a> )
+      <br />
       <ReactAutocomplete
         className="awdaw"
         items={[
-          { id: 'foo', label: 'foo' },
-          { id: 'bar', label: 'bar' },
-          { id: 'baz', label: 'baz' },
+          { id: 'foo1', value: 'foo' },
+          { id: 'bar2', value: 'bar' },
+          { id: 'baz3', value: 'baz' },
         ]}
         shouldItemRender={(item, value) =>
-          item.label.toLowerCase().indexOf(value.toLowerCase()) > -1
+          item.value.toLowerCase().indexOf(value.toLowerCase()) > -1
         }
-        getItemValue={(item) => item.label}
-        renderInput={(props) => <input className="form-control" {...props} />}
+        getItemValue={(item) => item.value}
+        renderInput={(props) => (
+          <input className="form-control mb-0 mx" {...props} />
+        )}
         renderItem={(item, highlighted) => (
           <div
             key={item.id}
-            style={{ backgroundColor: highlighted ? '#eee' : 'transparent' }}
+            style={{
+              backgroundColor: highlighted ? '#55973f' : 'transparent',
+              color: highlighted ? 'white' : 'black',
+              textAlign: 'left',
+              padding: '8px',
+            }}
           >
-            {item.label}
+            {item.value}
           </div>
         )}
         value={state.value}
-        onChange={(e) => setState({ value: e.target.value })}
-        onSelect={(value) => setState({ value })}
+        onChange={(e) => setState({ value: e.target.value, id: '' })}
+        onSelect={onSelect}
       />
+      <a
+        className="primary-button  btn-lg rounded-pill mt-4 ms-0 ms-sm-4"
+        role="button"
+      >
+        Add
+      </a>
     </div>
   );
 };
