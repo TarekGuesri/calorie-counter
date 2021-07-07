ddf89b9da2c7913f6f23e72519439fbc7c2479f1
TarekGuesri <tguesri@gmail.com>
2021-07-07 21:22:56 +0100

    Updated link

--- client/src/components/today-calories/CaloriesStats.js
+++ client/src/components/today-calories/CaloriesStats.js
@@ -37,7 +37,7 @@ const CaloriesStats = ({ totalCalories }) => {
               <td className="text-center pb-3" colSpan="2">
                 In order to get the estimation of the number of calories you
                 need to consume, you need to complete your profile{' '}
-                <a href="/">here</a>
+                <a href="/profile">here</a>
               </td>
             </tr>
           )}
