4bd4146afd479ae265f2fd9e6615903fa99996c7
TarekGuesri <tguesri@gmail.com>
2021-07-11 02:20:16 +0100

    Updated User model

--- models/User.js
+++ models/User.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  profile: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Profile',
+  },
 });
 
 module.exports = mongoose.model('User', userSchema);
