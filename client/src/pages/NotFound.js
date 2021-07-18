65ab0a07fe6bf159b4fce0d60f0840946a1814e2
TarekGuesri <tguesri@gmail.com>
2021-07-18 21:32:53 +0100

    Updated not found page's title

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+
+import { WEBSITE_NAME } from 'src/utils/brand';
 
 const NotFound = () => {
-  return <h1>Page not found</h1>;
+  return (
+    <>
+      <Helmet>
+        <title>{`${WEBSITE_NAME} - Not Found`}</title>
+      </Helmet>
+      <h1>Page not found</h1>
+    </>
+  );
 };
 
 export default NotFound;
