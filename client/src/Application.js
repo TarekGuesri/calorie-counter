05165e6cb8e2ebaffb4bcd2a7d32bf63162d0482
TarekGuesri <tguesri@gmail.com>
2021-07-18 20:53:56 +0100

    Made profile page private

--- client/src/Application.js
+++ client/src/Application.js
@@ -14,7 +14,7 @@ const Application = () => {
   return (
     <div className="main-layout text-center p-5 m-5 mx-auto bg-light">
       <Switch>
-        <Route path="/profile" component={ProfilePage} />
+        <PrivateRoute path="/profile" component={ProfilePage} />
         <PrivateRoute path="/today" component={TodayCaloriesPage} />
         <PrivateRoute path="/my-foods" component={MyFoodsPage} />
         <Route component={NotFoundPage} />
