d3919e921df75fdc39d2d68a747f781d8f320d22
TarekGuesri <tguesri@gmail.com>
2021-07-19 19:28:41 +0100

    Added more tips

--- client/src/components/tips/TipList.js
+++ client/src/components/tips/TipList.js
@@ -34,7 +34,40 @@ const TipList = () => {
       <br />
       <br />
 
-      <li>2nd item</li>
+      <li>
+        A proper diet and exercise is the most common and accepted way to lose
+        weight. Though, it is not recommended to lower your calorie intake by
+        more than 1000 calories per day{' '}
+        <span className="text-muted">(Losing 1 kg per week) </span>
+        which can be unhealthy, because it can result to a muscle loss which in
+        turn lowers your metabolism.
+      </li>
+
+      <br />
+      <br />
+
+      <li>
+        There are many approaches to weight loss and there is no set ideal
+        method that works for all people, which is why so many different diets
+        and exercise regimens exist. While some methods are more effective for
+        each individual person, not all weight loss methods are equivalent, and
+        studies suggest that some approaches are healthier than others.
+      </li>
+
+      <br />
+      <br />
+
+      <li>
+        A lot people confuse bloating with fat. Bloating occurs when your belly
+        becomes enlarged with fluid or gas and not because you are having more
+        calories than you need. It happens when you eat foods that are hard for
+        your stomach to digest{' '}
+        <span className="text-muted">
+          (Such as legumes, dairy, apples, salty foods...etc){' '}
+        </span>
+        . In order to avoid bloating, it is better to drink more water and try a
+        different diet that does not contain high-FODMAP foods.
+      </li>
     </ul>
   );
 };
